9b6488d56bdc3cfe69b42602aedba36c3da89115
PavelSZerocod <samusenko.pasha@gmail.com>
2024-08-15 12:54:01 +0300

    add new name file "target.png"

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра - ТИР")
-icon = pygame.image.load("main/img/иконка.png")
+icon = pygame.image.load("img/иконка.png")
 pygame.display.set_icon(icon)
 
-target_img = pygame.image.load("img/target.png")
+target_img = pygame.image.load("img/target.png.png")
 target_width = 80
 target_height = 80
 
